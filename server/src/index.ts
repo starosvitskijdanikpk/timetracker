65a358f7d6ef44ef3c17e6669056377a1d6d8d4a
starosvitskijdanikpk <starosvitskijdanikpk@gmail.com>
2026-02-18 21:19:22 +0200

    Fix server\src\index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ import { app } from "./app.js";
 const port = Number(process.env.PORT) || 10000;
 const host = "0.0.0.0";
 
-app.listen(port, host, () => {
-  // Server started
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
 
